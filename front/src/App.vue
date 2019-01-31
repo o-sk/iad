e20dad00e56cd33eb441ddff6e452ece9d0504c0
o-sk <m.ohsuka@gmail.com>
2019-01-31 22:26:27 +0900

    Mark up with pug

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,65 +1,24 @@
-<template>
-  <v-app>
-    <v-container>
-      <v-layout row wrap>
-        <v-flex xs12 sm6 md3 offset-md3>
-          <v-text-field
-            label="query1"
-            v-model="query1"
-            @keypress.enter="submitQuery1"
-            @change="submitQuery1"
-            :loading="loading1"
-            outline
-          ></v-text-field>
-        </v-flex>
-        <v-flex xs12 sm6 md3>
-          <v-text-field
-            label="query2"
-            v-model="query2"
-            @keypress.enter="submitQuery2"
-            @change="submitQuery2"
-            :loading="loading2"
-            outline
-          ></v-text-field>
-        </v-flex>
-      </v-layout>
+<template lang="pug">
+  v-app
+    v-container
+      v-layout(row wrap)
+        v-flex(xs12 sm6 md3 offset-md3)
+          v-text-field(label="query1" v-model="query1" @keypress.enter="submitQuery1" @change="submitQuery1"
+            :loading="loading1" outline)
+        v-flex(xs12 sm6 md3)
+          v-text-field(label="query2" v-model="query2" @keypress.enter="submitQuery2" @change="submitQuery2"
+            :loading="loading2" outline)
 
-      <v-layout>
-        <v-flex xs12 sm12 md12>
-          <v-card>
-            <v-container grid-list-sm fluid>
-              <v-layout row wrap>
-                <v-flex
-                  v-for="image in imageList"
-                  :key="image.id + image.rh"
-                  xs4 sm4 md2
-                  d-flex
-                >
-                  <v-card flat tile class="d-flex">
-                    <v-img
-                      :src="image.tu"
-                      aspect-ratio="1"
-                      class="grey lighten-2"
-                    >
-                      <v-layout
-                        slot="placeholder"
-                        fill-height
-                        align-center
-                        justify-center
-                        ma-0
-                      >
-                        <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                      </v-layout>
-                    </v-img>
-                  </v-card>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card>
-        </v-flex>
-      </v-layout>
-    </v-container>
-  </v-app>
+      v-layout
+        v-flex(xs12 sm12 md12)
+          v-card
+            v-container(grid-list-sm fluid)
+              v-layout(row wrap)
+                v-flex(v-for="image in imageList" :key="image.id + image.rh" xs4 sm4 md2 d-flex)
+                  v-card(flat tile class="d-flex")
+                    v-img(:src="image.tu" aspect-ratio="1" class="grey lighten-2")
+                      v-layout(slot="placeholder" fill-height align-center justify-center ma-0)
+                        v-progress-circular(indeterminate color="grey lighten-5")
 </template>
 
 <script lang="ts">
