3055f61efbd82d1575f8b6d36927e192448fd045
o-sk <m.ohsuka@gmail.com>
2019-02-02 11:33:24 +0900

    Refactor ShowCase

--- front/src/App.vue
+++ front/src/App.vue
@@ -9,22 +9,16 @@
 
       v-layout
         v-flex(xs12 sm12 md12)
-          v-card
-            v-container(grid-list-sm fluid)
-              v-layout(row wrap)
-                v-flex(v-for="image in imageList" :key="image.id + image.rh" xs4 sm4 md2 d-flex)
-                  v-card(flat tile class="d-flex")
-                    v-img(:src="image.tu" aspect-ratio="1" class="grey lighten-2")
-                      v-layout(slot="placeholder" fill-height align-center justify-center ma-0)
-                        v-progress-circular(indeterminate color="grey lighten-5")
+          show-case(:imageList1="resultList1" :imageList2="resultList2")
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import QueryField from '@/components/QueryField.vue';
+import ShowCase from '@/components/ShowCase.vue';
 
 @Component({
-  components: { QueryField },
+  components: { QueryField, ShowCase },
 })
 export default class App extends Vue {
   private resultList1: any[];
@@ -52,21 +46,6 @@ export default class App extends Vue {
 
   private fail2() {
     this.resultList2 = [];
-  }
-
-  get imageList() {
-    const imageList: any[] = [];
-    const maxLength: number = (this.resultList1.length > this.resultList2.length) ?
-      this.resultList1.length : this.resultList2.length;
-    for (let i = 0; i < maxLength; i++) {
-      if (this.resultList1[i]) {
-        imageList.push(this.resultList1[i]);
-      }
-      if (this.resultList2[i]) {
-        imageList.push(this.resultList2[i]);
-      }
-    }
-    return imageList;
   }
 }
 </script>
---
+++ front/src/components/ShowCase.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  v-card
+    v-container(grid-list-sm fluid)
+      v-layout(row wrap)
+        v-flex(v-for="image in imageList" :key="image.id + image.rh" xs4 sm4 md2 d-flex)
+          v-card(flat tile class="d-flex")
+            v-img(:src="image.tu" aspect-ratio="1" class="grey lighten-2")
+              v-layout(slot="placeholder" fill-height align-center justify-center ma-0)
+                v-progress-circular(indeterminate color="grey lighten-5")
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class ShowCase extends Vue {
+  @Prop() private imageList1!: any[];
+  @Prop() private imageList2!: any[];
+
+  get imageList() {
+    const imageList: any[] = [];
+    const maxLength: number = (this.imageList1.length > this.imageList2.length) ?
+      this.imageList1.length : this.imageList2.length;
+    for (let i = 0; i < maxLength; i++) {
+      if (this.imageList1[i]) {
+        imageList.push(this.imageList1[i]);
+      }
+      if (this.imageList2[i]) {
+        imageList.push(this.imageList2[i]);
+      }
+    }
+    return imageList;
+  }
+}
+</script>
