016f0fa515a486074fa58ab547e2d480b7008736
o-sk <m.ohsuka@gmail.com>
2019-02-11 23:12:03 +0900

    Fix warning

--- front/tests/unit/App.spec.ts
+++ front/tests/unit/App.spec.ts
@@ -1,5 +1,9 @@
 import { mount } from '@vue/test-utils';
+import Vue from 'vue';
+import Vuetify from 'vuetify';
 import App from '@/App.vue';
+
+Vue.use(Vuetify);
 
 describe('App', () => {
   test('is a Vue instance', () => {
