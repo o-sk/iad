7e82ae040051098ddb7cb120966cbf5bc3376b98
o-sk <m.ohsuka@gmail.com>
2019-02-19 00:45:15 +0900

    Add ShowCase.spec.ts

---
+++ front/tests/unit/components/ShowCase.spec.ts
@@ -0,0 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
+import Vue from "vue";
+import Vuetify from "vuetify";
+import ShowCase from "@/components/ShowCase.vue";
+
+Vue.use(Vuetify);
+
+describe("ShowCase", () => {
+  test("propsData", () => {
+    const wrapper = shallowMount(ShowCase, {
+      propsData: {
+        imageList1: [1, 2],
+        imageList2: [11, 12]
+      }
+    });
+    expect(wrapper.vm.$props.imageList1).toEqual([1, 2]);
+    expect(wrapper.vm.$props.imageList2).toEqual([11, 12]);
+  });
+});
